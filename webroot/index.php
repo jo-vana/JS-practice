9a6e013d1959eb7aa9760e7f6900e654ba905eb6
jo-vana <jovananukic85@gmail.com>
2018-01-30 11:59:39 +0100

    Extra for first commit

--- webroot/index.php
+++ webroot/index.php
@@ -6,6 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP practices</title>
+
+    <!-- JS-->
+    <script src="variables.js" type="text/javascript"></script>
 </head>
 <body>
 <h1>This is my JS practice project</h1>
